040fbb18a3efe26571fcffbe63d56388c4f63637
jskno <lodmnos@gmail.com>
2024-12-26 23:38:59 +0100

    WebResource MVC

---
+++ starting_guide/my-resources-app/src/main/java/com/jskno/resources/app/MyResourcesAppApplication.java
@@ -0,0 +1,13 @@
+package com.jskno.resources.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyResourcesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyResourcesAppApplication.class, args);
+    }
+
+}
---
+++ starting_guide/my-resources-app/src/test/java/com/jskno/resources/app/MyResourcesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jskno.resources.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyResourcesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
