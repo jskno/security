ffb0de742eb445d8634bb151153b5f9d02ab89a4
josecano <jose.cano@europcar.com>
2025-01-31 17:55:40 +0100

    Backend for SPA project

---
+++ OAuth2InSpringApplications/BackEnd_For_Spa_Client/spring-auth-server/src/test/java/com/jskno/spring/auth/server/SpringAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jskno.spring.auth.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
